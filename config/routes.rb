1d2092d44a4baceee78479c40fb596245b207518
mark nordquist <markanordquist@gmail.com>
2020-09-22 16:16:00 -0500

    Commit Update for creating table for articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resource :articles, only: [:show]
+  resource :articles, only: [:show, :index]
 end
