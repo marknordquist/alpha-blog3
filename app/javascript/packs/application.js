8c290a19de322e519b8cc31b4b7908f05af73e57
mark nordquist <markanordquist@gmail.com>
2020-10-08 04:35:04 -0500

    Working with Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -15,3 +20,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap"
