79f62b867aa9c5d326948e1888eadaab1ccb50bb
mark nordquist <markanordquist@gmail.com>
2020-10-14 16:29:51 -0500

    Get ready for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,9 +53,9 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user and !current_user.admin?
-      flash[:danger] = "You can only edit or delete your own articles"
-      redirect_to root_path
+    if current_user != @article.user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
     end
   end
 
