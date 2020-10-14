483324f7bc53c4c536f58fc7e73e5c0d0ea1e1e3
mark nordquist <markanordquist@gmail.com>
2020-10-14 09:19:23 -0500

    This after adding User security

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,75 +1,62 @@
-
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
-  # GET /articles
-  # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 
-  # GET /articles/1
-  # GET /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
   def new
     @article = Article.new
   end
 
-  # GET /articles/1/edit
   def edit
   end
 
-  # POST /articles
-  # POST /articles.json
-
-
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
-      flash[:notice] = "Article was created successfully."
-      redirect_to @article
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
     else
       render 'new'
     end
   end
 
-
-  # PATCH/PUT /articles/1
-  # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /articles/1
-  # DELETE /articles/1.json
+  def show
+  end
+
   def destroy
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :description)
+  def article_params
+    params.require(:article).permit(:title, :description, category_ids: [])
+  end
+
+  def require_same_user
+    if current_user != @article.user and !current_user.admin?
+      flash[:danger] = "You can only edit or delete your own articles"
+      redirect_to root_path
     end
+  end
+
 end
